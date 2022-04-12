807ae1084ca32809573c3d2f405aa91eef1b0535
Sabir82 <ahmadsabir796@gmail.com>
2022-04-12 14:35:13 +0530

    push

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,159 +1,215 @@
 import React, { useState, useRef, useEffect, useReducer } from "react";
-
-import Link from "next/link";
 import Image from "next/image";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import { AppBar, Grid, Toolbar, Typography } from "@material-ui/core";
+import { AppBar,makeStyles,CssBaseline,  Grid, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import "../../styles/navBar.module.css"
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 // import { useTranslation } from "react-i18next";
 // import "../../services/localizationService";
+// const propTypes = {
+//   navPosition: PropTypes.string,
+//   hideNav: PropTypes.bool,
+//   hideSignin: PropTypes.bool,
+//   hideSignup: PropTypes.bool,
+//   bottomOuterDivider: PropTypes.bool,
+//   bottomDivider: PropTypes.bool,
+// };
 
-const propTypes = {
-  navPosition: PropTypes.string,
-  hideNav: PropTypes.bool,
-  hideSignin: PropTypes.bool,
-  hideSignup: PropTypes.bool,
-  bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool,
-};
+// const defaultProps = {
+//   navPosition: "",
+//   hideNav: false,
+//   hideSignin: false,
+//   hideSignup: false,
+//   bottomOuterDivider: false,
+//   bottomDivider: false,
+// };
 
-const defaultProps = {
-  navPosition: "",
-  hideNav: false,
-  hideSignin: false,
-  hideSignup: false,
-  bottomOuterDivider: false,
-  bottomDivider: false,
-};
+// const Header = ({
+//   className,
+//   navPosition,
+//   hideNav,
+//   hideSignin,
+//   hideSignup,
+//   bottomOuterDivider,
+//   bottomDivider,
+//   ...props
+// }) => {
+//   const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-const Header = ({
-  className,
-  navPosition,
-  hideNav,
-  hideSignin,
-  hideSignup,
-  bottomOuterDivider,
-  bottomDivider,
-  ...props
-}) => {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+//   const [isActive, setIsactive] = useState(false);
 
-  const [isActive, setIsactive] = useState(false);
+//   const nav = useRef(null);
+//   const hamburger = useRef(null);
 
-  const nav = useRef(null);
-  const hamburger = useRef(null);
+//   useEffect(() => {
+//     isActive && openMenu();
+//     document.addEventListener("keydown", keyPress);
+//     document.addEventListener("click", clickOutside);
+//     return () => {
+//       document.removeEventListener("keydown", keyPress);
+//       document.removeEventListener("click", clickOutside);
+//       closeMenu();
+//     };
+//   }, []);
 
-  useEffect(() => {
-    isActive && openMenu();
-    document.addEventListener("keydown", keyPress);
-    document.addEventListener("click", clickOutside);
-    return () => {
-      document.removeEventListener("keydown", keyPress);
-      document.removeEventListener("click", clickOutside);
-      closeMenu();
-    };
-  }, []);
+//   // useLayoutEffect(() => {
+//   //   window.addEventListener('click', forceUpdate);
 
-  // useLayoutEffect(() => {
-  //   window.addEventListener('click', forceUpdate);
+//   //   return () => window.removeEventListener('click', forceUpdate);
+//   // }, []);
 
-  //   return () => window.removeEventListener('click', forceUpdate);
-  // }, []);
+//   const openMenu = () => {
+//     document.body.classList.add("off-nav-is-active");
+//     nav.current.style.maxHeight = nav.current.scrollHeight + "px";
+//     setIsactive(true);
+//   };
 
-  const openMenu = () => {
-    document.body.classList.add("off-nav-is-active");
-    nav.current.style.maxHeight = nav.current.scrollHeight + "px";
-    setIsactive(true);
-  };
+//   const closeMenu = () => {
+//     document.body.classList.remove("off-nav-is-active");
+//     nav.current && (nav.current.style.maxHeight = null);
+//     setIsactive(false);
+//   };
 
-  const closeMenu = () => {
-    document.body.classList.remove("off-nav-is-active");
-    nav.current && (nav.current.style.maxHeight = null);
-    setIsactive(false);
-  };
+//   const logout = () => {
+//     document.body.classList.remove("off-nav-is-active");
+//     nav.current && (nav.current.style.maxHeight = null);
+//     setIsactive(false);
+//     localStorage.removeItem("uid");
+//     localStorage.removeItem("signup");
+//   };
 
-  const logout = () => {
-    document.body.classList.remove("off-nav-is-active");
-    nav.current && (nav.current.style.maxHeight = null);
-    setIsactive(false);
-    localStorage.removeItem("uid");
-    localStorage.removeItem("signup");
-  };
+//   const profile = () => {
+//     document.body.classList.remove("off-nav-is-active");
+//     nav.current && (nav.current.style.maxHeight = null);
+//     setIsactive(false);
+//   };
 
-  const profile = () => {
-    document.body.classList.remove("off-nav-is-active");
-    nav.current && (nav.current.style.maxHeight = null);
-    setIsactive(false);
-  };
+//   const keyPress = (e) => {
+//     isActive && e.keyCode === 27 && closeMenu();
+//   };
 
-  const keyPress = (e) => {
-    isActive && e.keyCode === 27 && closeMenu();
-  };
+//   const clickOutside = (e) => {
+//     if (!nav.current) return;
+//     if (
+//       !isActive ||
+//       nav.current.contains(e.target) ||
+//       e.target === hamburger.current
+//     )
+//       return;
+//     closeMenu();
+//   };
 
-  const clickOutside = (e) => {
-    if (!nav.current) return;
-    if (
-      !isActive ||
-      nav.current.contains(e.target) ||
-      e.target === hamburger.current
-    )
-      return;
-    closeMenu();
-  };
+//   const classes = classNames(
+//     "site-header",
+//     bottomOuterDivider && "has-bottom-divider",
+//     className
+//   );
 
-  const classes = classNames(
-    "site-header",
-    bottomOuterDivider && "has-bottom-divider",
-    className
-  );
+//   const changeLanguage = (e) => {
+//     window.changeLanguage(e.target.dataset.language);
+//     window.addEventListener("click", forceUpdate);
+//   };
 
-  const changeLanguage = (e) => {
-    window.changeLanguage(e.target.dataset.language);
-    window.addEventListener("click", forceUpdate);
-  };
+//   const imgLoader = ({ src }) => {
+//     return "https://images.unsplash.com/photo-1549924231-f129b911e442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+//   };
 
-  const imgLoader = ({ src }) => {
-    return "https://images.unsplash.com/photo-1549924231-f129b911e442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
-  };
+// END NAVBAR //
+// NEW NAVBAR //
 
-  return (
-    <AppBar position="static" elevation={0}>
-      <Toolbar disableGutter>
-        <Grid container justifyContent="space-between">
-          {/* logo */}
-          <Grid item xs={4}>
-            <Image
-              loader={imgLoader}
-              src="https://images.unsplash.com/photo-1549924231-f129b911e442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-              alt=""
-              width={50}
-              height={50}
-            />
-          </Grid>
+  
+      // <AppBar position="static" elevation={0}>
+      //   <Toolbar disableGutter>
+      //     <Grid container justifyContent="space-between">
+      //       {/* logo */}
+      //       <Grid item xs={4}>
+      //         <Image
+      //           loader={imgLoader}
+      //           src="https://images.unsplash.com/photo-1549924231-f129b911e442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      //           alt=""
+      //           width={50}
+      //           height={50}
+      //         />
+      //       </Grid>
 
-          <Grid item xs={6}>
-            <Grid item container justifyContent="flex-end" alignItems="center">
-              <Grid item xs={2}>
-                <Link href="/">Home</Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link href="/login">Login</Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link href="/about">About</Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link href="/signup">Sign up</Link>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
-  );
-};
+      //       <Grid item xs={6}>
+      //         <Grid item container justifyContent="flex-end" alignItems="center">
+      //           <Grid item xs={2}>
+      //             <Link href="/">Home</Link>
+      //           </Grid>
+      //           <Grid item xs={2}>
+      //             <Link href="/login">Login</Link>
+      //           </Grid>
+      //           <Grid item xs={2}>
+      //             <Link href="/about">About</Link>
+      //           </Grid>
+      //           <Grid item xs={2}>
+      //             <Link href="/signup">Sign up</Link>
+      //           </Grid>
+      //         </Grid>
+      //       </Grid>
+      //     </Grid>
+      //   </Toolbar>
+      // </AppBar>
+      // const useStyles = makeStyles((theme) => ({
+      //   navlinks: {
+      //     marginLeft: theme.spacing(10),
+      //     display: "flex",
+      //   },
+      //  logo: {
+      //     flexGrow: "1",
+      //     cursor: "pointer",
+      //   },
+      //   link: {
+      //     textDecoration: "none",
+      //     color: "white",
+      //     fontSize: "20px",
+      //     marginLeft: theme.spacing(20),
+      //     "&:hover": {
+      //       color: "yellow",
+      //       borderBottom: "1px solid white",
+      //     },
+      //   },
+      // }));
+const Header=()=>{
+  // const classes = useStyles();
+  
 
-Header.propTypes = propTypes;
-Header.defaultProps = defaultProps;
-
+  return(
+  //   <AppBar position="static">
+  //   <CssBaseline />
+  //   <Toolbar>
+  //     <Typography variant="h4" className={classes.logo}>
+  //       Navbar
+  //     </Typography>
+  //       <div className={classes.navlinks}>
+  //         <Link to="/" className={classes.link}>
+  //           Home
+  //         </Link>
+  //         <Link to="/about" className={classes.link}>
+  //           About
+  //         </Link>
+  //         <Link to="/contact" className={classes.link}>
+  //           Contact
+  //         </Link>
+  //         <Link to="/faq" className={classes.link}>
+  //           FAQ
+  //         </Link>
+  //       </div>
+  //   </Toolbar>
+  // </AppBar>  
+  
+// Header.propTypes = propTypes;
+// Header.defaultProps = defaultProps;
+ <></> )}
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,30 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import { wrapper } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return( 
+  <>
+  <Head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <script src="https://kit.fontawesome.com/b690895109.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.10.0/css/lightgallery.css" />
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+    crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.10.0/js/lightgallery.min.js"></script>
+  </Head>
+  <Component {...pageProps} />;
+  </>
+  )
 }
 
 export default wrapper.withRedux(MyApp);
